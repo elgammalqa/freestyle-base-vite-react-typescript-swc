6ae65e240f83b2cbd139bd09b30463db072621b7
elgammalqa <elgammalqa@gmail.com>
2026-03-31 18:27:00 -0500

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,9 +6,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
     allowedHosts: true,
-    host: "0.0.0.0"
+    host: "0.0.0.0",
   },
 });
